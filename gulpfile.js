9343a58a1b70e5eec1ca605e9aeda5fafc10af1d
Luigi Agosti <luigi.agosti@gmail.com>
2016-09-08 16:38:15 +0100

    Other pages optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var replace = require('gulp-replace');
 var inline = require('gulp-inline')
   , uglify = require('gulp-uglify')
   , minifyCss = require('gulp-minify-css');
@@ -6,31 +7,40 @@ var imagemin = require('gulp-imagemin');
 var googleWebFonts = require('gulp-google-webfonts');
 var options = {
     fontsDir: 'fonts/',
+    cssDir: 'css/'
 };
 
 gulp.task('default', ['css', 'images']);
 
-gulp.task('css', ['fonts'], function() {
-  return gulp.src('public/index.html')
+gulp.task('css', ['fontsCssPath'], function() {
+  return gulp.src('public/**/index.html')
     .pipe(inline({
       base: 'public/',
       js: uglify,
       css: minifyCss,
       disabledTypes: ['svg', 'img'],
-      ignore: ['/font-awesome/css/font-awesome.min.css', '/js/analytics.js']
+      ignore: ['/js/analytics.js']
     }))
-    .pipe(gulp.dest('public/'));
+    .pipe(gulp.dest(function(file) {
+      return file.base;
+    }));
 });
 
 gulp.task('images', () =>
-    gulp.src('public/img/clients/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('public/img/clients/'))
+  gulp.src('public/img/clients/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('public/img/clients/'))
 );
 
-gulp.task('fonts', function () {
+gulp.task('fontsCssPath', ['fonts'], function() {
+  gulp.src('public/css/fonts.css', {base : 'public/css/fonts.css'})
+    .pipe(replace('url(', 'url(/'))
+    .pipe(gulp.dest('public/css/fonts.css'))
+})
+
+gulp.task('fonts', function() {
   return gulp.src('./fonts.list')
 	 .pipe(googleWebFonts(options))
-	  .pipe(gulp.dest('public/fonts'))
+	 .pipe(gulp.dest('public'))
 	;
 });
