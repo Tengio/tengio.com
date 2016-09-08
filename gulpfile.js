b9803e377a05abbcf88803daeb67b8a6aee1b980
Luigi Agosti <luigi.agosti@gmail.com>
2016-09-09 00:16:23 +0100

    Gulp to execute all the flow

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,30 @@ var inline = require('gulp-inline')
   , minifyCss = require('gulp-minify-css');
 var imagemin = require('gulp-imagemin');
 var htmlmin = require('gulp-htmlmin');
+var shell = require('gulp-shell');
 var googleWebFonts = require('gulp-google-webfonts');
 var options = {
     fontsDir: 'fonts/',
     cssDir: 'css/'
 };
 
-gulp.task('default', ['minify']);
+gulp.task('prepare', shell.task([
+  'rm -rf public',
+  'hugo -s .'
+]))
 
-gulp.task('minify', ['css', 'images'], function() {
-  return gulp.src('public/**/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest(function(file) {
-      return file.base;
-    }));
+gulp.task('fonts', ['prepare'], function() {
+  return gulp.src('./fonts.list')
+	 .pipe(googleWebFonts(options))
+	 .pipe(gulp.dest('public'))
+	;
 });
+
+gulp.task('fontsCssPath', ['fonts'], function() {
+  gulp.src('public/css/fonts.css', {base : 'public/css/fonts.css'})
+    .pipe(replace('url(', 'url(/'))
+    .pipe(gulp.dest('public/css/fonts.css'))
+})
 
 gulp.task('css', ['fontsCssPath'], function() {
   return gulp.src('public/**/index.html')
@@ -35,7 +44,7 @@ gulp.task('css', ['fontsCssPath'], function() {
     }));
 });
 
-gulp.task('images', function() {
+gulp.task('images', ['css'], function() {
   gulp.src('public/img/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest(function(file) {
@@ -43,15 +52,12 @@ gulp.task('images', function() {
     }));
 });
 
-gulp.task('fontsCssPath', ['fonts'], function() {
-  gulp.src('public/css/fonts.css', {base : 'public/css/fonts.css'})
-    .pipe(replace('url(', 'url(/'))
-    .pipe(gulp.dest('public/css/fonts.css'))
-})
-
-gulp.task('fonts', function() {
-  return gulp.src('./fonts.list')
-	 .pipe(googleWebFonts(options))
-	 .pipe(gulp.dest('public'))
-	;
+gulp.task('minify', ['images'], function() {
+  return gulp.src('public/**/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest(function(file) {
+      return file.base;
+    }));
 });
+
+gulp.task('default', ['minify']);
